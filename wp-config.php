a3d5f01e275cfa1a8128b3c4686f906138534485
Anup Biswas <anup04sust@gmail.com>
2015-08-21 00:10:59 +0600

    ubuntu start

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'wordpress_singlepage');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
